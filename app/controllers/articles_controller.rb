592360f1002286d150c1179345f108ec351700d9
Peter Muturi <pmuturi65@gmail.com>
2015-08-23 21:13:17 +0300

    index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
 
+	def index
+    @articles = Article.all
+  end
+
 	 def show
     @article = Article.find(params[:id])
   end
@@ -18,3 +22,4 @@ private
   def article_params
     params.require(:article).permit(:title, :text)
   end
+end
