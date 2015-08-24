408bd0b1175db89eb8434c2ef29af48b5e8995cf
Peter Muturi <pmuturi65@gmail.com>
2015-08-24 08:40:34 +0300

    update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,16 @@ else
 	 render 'new'
   end
 end
+
+def update
+  @article = Article.find(params[:id])
+ 
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
  
 private
   def article_params
